bb717ac7e534686f50b5b1edc907ee4c2ea20820
mstanka <mice78.mm@gmail.com>
2021-04-05 09:09:34 +0200

    fix minutka2 - negative values, disable btn

--- 20210410-Javascript-8/minutka2/index.js
+++ 20210410-Javascript-8/minutka2/index.js
@@ -12,16 +12,19 @@ let ringing;
 const countDown = () => {
   time -= 1;
   secondsEl.textContent = String(time).padStart(2, '0');
+  btnEl.disabled = true;
 
   if (time === 0) {
     clearInterval(ringing);
     audioEl.play();
     alarmEl.classList.add('alarm--ring');
     timeInputEl.value = '';
+    btnEl.disabled = false;
   }
 };
 
 btnEl.addEventListener('click', () => {
   time = Number(timeInputEl.value);
+  if (!time || time < 0) return;
   ringing = setInterval(countDown, 1000);
 });
