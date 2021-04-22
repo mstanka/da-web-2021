a081557156ba0cb1a36a6d1166d85287cf9adc21
mstanka <mice78.mm@gmail.com>
2021-04-22 20:24:44 +0200

    update js-p-4 quote

--- 20210421-Javascript-p-4/quotes/src/index.js
+++ 20210421-Javascript-p-4/quotes/src/index.js
@@ -2,7 +2,7 @@ import Quote from './Quote/index.js';
 import './style.css';
 
 const quote = {
-  text: 'Try to understand the wind. No the laughter.',
+  text: 'Try to understand the wind. Not the laughter.',
   author: 'Unknown',
 };
 
